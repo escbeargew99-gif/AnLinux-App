6ef9ac2765d5f7785ef59ceb059aef81366dce6e
BigBearHub29 <escbeargew99@gmail.com>
2026-01-12 08:55:02 +0700

    Create Configure.js

---
+++ DIY/Configure.js
@@ -0,0 +1,3 @@
+# Create the runner and start the configuration experience
+$ ./config.sh --url https://github.com/escbeargew99-gif/AnLinux-App --token B3TJNLBDBAQE3TQZKML34RDJMRQXM# Last step, run it!
+$ ./run.sh
